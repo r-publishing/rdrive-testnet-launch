87f1f932aeeb8ef3182c5a4fc977eb30d8808317
Bill-Kunj <56281673+Bill-Kunj@users.noreply.github.com>
2022-06-22 06:03:44 -0400

    Update gen-wallets.bash

--- gen-wallets.bash
+++ gen-wallets.bash
@@ -2,8 +2,10 @@
 TREASURY=testnet-launch.treasury.wallets.txt
 AIRDROP=testnet-launch.wallets.txt
 RCHAIN_TREASURY=testnet-launch.rchain-treasury.wallets.txt
+PROPOSE=testnet-launch.propose.wallets.txt
 wget https://raw.githubusercontent.com/r-publishing/rdrive-testnet-launch/master/$RCHAIN_TREASURY
 wget https://raw.githubusercontent.com/r-publishing/rdrive-testnet-launch/master/$TREASURY
 wget https://raw.githubusercontent.com/r-publishing/rdrive-testnet-launch/master/$AIRDROP
+wget https://raw.githubusercontent.com/r-publishing/rdrive-testnet-launch/master/$PROPOSE
 
-cat $TREASURY $AIRDROP $RCHAIN_TREASURY > wallets.txt
+cat $TREASURY $AIRDROP $RCHAIN_TREASURY $PROPOSE > wallets.txt
